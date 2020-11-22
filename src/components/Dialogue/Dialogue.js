8659733febe277e5970d95c36a53fc631a985d3d
DaunPony <ealtynbek089@gmail.com>
2020-11-22 17:10:09 +0600

    Added NavLink

--- src/components/Dialogue/Dialogue.js
+++ src/components/Dialogue/Dialogue.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import styles from './Dialogue.module.css'
 
 const Dialogue = () => {
     return (
-        <div>
-            Dialogue
+        <div className={styles.dialogueWrapper}>
+            <div className="dialogueUser">
+                <div className="author">
+                    Vlad
+                </div>
+                <div className="author">
+                    Pipe
+                </div>
+            </div>
+            <div className="dialogueMessages">
+                <div className="message">
+                    Random
+                </div>
+                <div className="message">
+                    Lap
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './NavigationBar.module.css'
 
 const NavigationBar = () => {
     return (
         <nav className={styles.leftBar}>
             <div className={`${styles.leftBar} ${styles.active}`}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile" activeClassName={styles.leftBarActive}>Profile</NavLink>
+
             </div>
-            <div className={`${styles.leftBar} ${styles.active}`}>
-                <a href="/dialogs">Messages</a>
+            <div className={styles.leftBar}>
+                <NavLink to="/dialogs" activeClassName={styles.leftBarActive}>Dialogs</NavLink>
             </div>
-            <div>
+            <div className={styles.leftBar}>
                 <a>News</a>
             </div>
-            <div>
+            <div className={styles.leftBar}>
                 <a>Music</a>
             </div>
-            <div>
+            <div className={styles.leftBar}>
                 <a>Settings</a>
             </div>
         </nav>
