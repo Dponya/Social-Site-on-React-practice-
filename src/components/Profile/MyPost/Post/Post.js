2f1be02b1896f7b11173c4a35d53b2230e3bc25c
DaunPony <ealtynbek089@gmail.com>
2020-12-10 19:15:07 +0600

    fix: fixed rerendering of component post with react.memo

--- src/components/Profile/MyPost/Post/Post.js
+++ src/components/Profile/MyPost/Post/Post.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Post.module.css';
 
-const Post = (props) => {
+const Post = React.memo((props) => {
+    console.log('render')
     return (
         <div className={styles.post}>
             <img src="https://i.mycdn.me/image?id=547153836623&plc=WEB&tkn=*jDsHLSHBPk3k6AjJYNnco-xwO3U&fn=sqr_288" />
             {props.message}
         </div>
     )
-}
+});
 
 export default Post;
